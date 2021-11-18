de87c0ac2d0f2e19cf2c9f9f1cd843ce67374f5d
can-git <canyil97@hotmail.com>
2021-11-18 15:45:23 +0300

    Adding Recording script to prevent synchronous of frames

--- Assets/Scripts/JsonThings.cs
+++ Assets/Scripts/JsonThings.cs
@@ -2,27 +2,49 @@
 using UnityEngine;
 using Newtonsoft.Json;
 using System.IO;
+using UnityEditor.Recorder;
+using UnityEditor.Recorder.Input;
 
 public class JsonThings : MonoBehaviour
 {
     List<JsonVehicleDatas> list;
     List<JsonDatas> genelList;
     JsonDatas jsonData;
-    int num = -1;
+    int num = 0;
     bool isLoaded = false;
+    private RecorderControllerSettings controllerSettings;
+    private RecorderController TestRecorderController;
+
+    [Header("Recording Config")]
+    public string scene_name = "aa3";
+    public string path = "D:\\Dosyalar\\Bi_alametler\\Dataset\\Recordings\\Datasets\\";
+    public int frameRate = 30;
+    public int Width = 1920;
+    public int Height = 1080;
+    public int frameStart = 0;
+    public int frameEnd = 5;
+
+
 
     private void Awake()
     {
-
         genelList = new List<JsonDatas>();
+        controllerSettings = ScriptableObject.CreateInstance<RecorderControllerSettings>();
+        TestRecorderController = new RecorderController(controllerSettings);
+        StartRecorder();
     }
 
     private void Update()
     {
         if (isLoaded)
         {
-            num++;
+            print(num);
+            if (num == frameEnd)
+            {
+                StopRecorder();
+            }
             WriteJson();
+            num++;
         }
     }
 
@@ -51,10 +73,44 @@ public class JsonThings : MonoBehaviour
         return list;
     }
 
-    private void OnApplicationQuit()
+
+    void StartRecorder()
     {
-        Debug.Log(Application.dataPath + "/VehiclesJsonData" + ".json");
-        File.WriteAllText(Application.dataPath + "/VehiclesJsonData" + ".json", JsonConvert.SerializeObject(genelList, Formatting.Indented));
+        var imageRecorder = ScriptableObject.CreateInstance<ImageRecorderSettings>();
+        imageRecorder.name = "SingleFrameRecorder";
+        imageRecorder.Enabled = true;
+        imageRecorder.CaptureAlpha = false;
+        imageRecorder.RecordMode = RecordMode.SingleFrame;
+        imageRecorder.OutputFormat = ImageRecorderSettings.ImageRecorderOutputFormat.PNG;
+        imageRecorder.OutputFile = path + scene_name + "\\Masks\\<Frame>";
+
+        imageRecorder.imageInputSettings = new CameraInputSettings
+        {
+            Source = ImageSource.MainCamera,
+            RecordTransparency = true,
+            CaptureUI = false,
+            FlipFinalOutput = false,
+            OutputWidth = Width,
+            OutputHeight = Height,
+        };
+
+
+        controllerSettings.SetRecordModeToFrameInterval(frameStart, frameEnd);
+        controllerSettings.AddRecorderSettings(imageRecorder);
+        controllerSettings.FrameRate = frameRate;
+
+        RecorderOptions.VerboseMode = false;
+        TestRecorderController.PrepareRecording();
+        TestRecorderController.StartRecording();
+    }
+
+    void StopRecorder()
+    {
+        TestRecorderController.StopRecording();
+        File.WriteAllText("D:/Dosyalar/Bi_alametler/Dataset/Recordings/Datasets/" + scene_name + "/VehiclesJsonData" + ".json", JsonConvert.SerializeObject(genelList, Formatting.Indented));
+        UnityEditor.EditorApplication.isPlaying = false;
+        Debug.Log("Recording is Finished");
+        Application.Quit();
     }
 }
 
